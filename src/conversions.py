6c204b77d735d0792c4b8dab3cbe2fe931958c12
jmaeagle99 <44687433+jmaeagle99@users.noreply.github.com>
2025-09-09 14:33:14 -0700

    Add node splitting

--- src/conversions.py
+++ src/conversions.py
@@ -1,8 +1,10 @@
+from pyparsing import List
+from htmlnode import HtmlNode
 from leafnode import LeafNode
 from textnode import TextNode, TextType
 
 
-def text_node_to_html_node(text_node: TextNode):
+def text_node_to_html_node(text_node: TextNode) -> HtmlNode:
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
     elif text_node.text_type == TextType.BOLD:
@@ -17,3 +19,19 @@ def text_node_to_html_node(text_node: TextNode):
         return LeafNode("img", "", props={"src": text_node.url, "alt": text_node.text})
     else:
         raise ValueError(f"Unknown TextType: {text_node.text_type}")
+
+def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: str, text_type: TextType) -> List[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            parts = node.text.split(delimiter)
+            if len(parts) % 2 == 0:
+                raise ValueError("invalid Markdown syntax: unmatched delimiter")
+            for index, part in enumerate(parts):
+                if index % 2 == 1:
+                    new_nodes.append(TextNode(part, text_type))
+                else:
+                    new_nodes.append(TextNode(part, TextType.TEXT))
+        else:
+            new_nodes.append(node)
+    return new_nodes
